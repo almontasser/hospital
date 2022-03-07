cf587a8c08a844167f8cb3228ce633875505a645
asmamuftah <99642820+asmamuftah@users.noreply.github.com>
2022-03-07 12:46:47 +0200

    asma muftah belied
    السلام عليكم تم اتمام المهمه المطلوبة

--- index.php
+++ index.php
@@ -65,7 +65,7 @@ include "header.php";
 
         <div class="row">
           <div class="col-xl-5 col-lg-6 video-box d-flex justify-content-center align-items-stretch position-relative">
-            <a href="https://www.youtube.com/watch?v=BpzIyqLafEU" class="glightbox play-btn mb-4"></a>
+            <a href="https://www.youtube.com/watch?v=bLt8BYPkkcQ" class="glightbox play-btn mb-4"></a>
           </div>
 
           <div class="col-xl-7 col-lg-6 icon-boxes d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-5">
@@ -126,9 +126,9 @@ include "header.php";
 
           <div class="col-lg-3 col-md-4">
             <div class="gallery-item">
-              <a href="c/xampp/htdocs/hospital/img/gallery-1.jpg" class="galelry-lightbox">
-                <img src="c/xampp/htdocs/hospital/img/gallery-1.jpg" alt="" class="img-fluid">
-                <img src="
+              <a href="assets/img/gallery/gallery-2.jpg" class="galelry-lightbox">
+                <img src="assets/img/gallery/gallery-2.jpg" alt="" class="img-fluid">
+               
               </a>
             </div>
           </div>
